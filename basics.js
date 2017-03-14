a85d0ad77e5d94e6f914b09f1b02eacb4e91d581
Dakota <bazookaharwell@gmail.com>
2017-03-13 16:13:34 -1000

    Test 1

---
+++ basics.js
@@ -0,0 +1,26 @@
+/* Create a `myName` variable and assign it a String value */
+
+var myName = "String";
+
+/* Create a `person` variable and give it 2 properties,
+ * `name`, assign it the same name as before,
+ * as well as an `age` (number);
+ */
+
+/* Create a variable called `canDrive`,
+ * if it should be true if your person object is at least 16 years old
+ */
+
+/* Create a function called `greet`,
+ * it should take a 1 parameter, `name`
+ * and it should print "Hello, my name is {name}"
+ */
+
+/* Create an array called `dataTypes` with atleast 1 of every data type;
+ * (there are 6 different data types);
+ */
+
+/* Create a `dog` object
+ * it should have a `bark` function that makes your dog bark!
+ * It should also have a name attribute with the value of 'Spot'
+ */
